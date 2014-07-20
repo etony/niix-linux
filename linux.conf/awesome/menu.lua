8bd31c550e2a0863107d782aac5e62cfba491a1f
etony C.F.AN <etony.an@gmail.com>
2014-07-20 20:31:11 +0800

    add menu.lua

---
+++ linux.conf/awesome/menu.lua
@@ -0,0 +1,19 @@
+xdgmenu = function(terminal)
+  return {
+    {'Internet',  {
+        {'Google Chrome', '/usr/bin/google-chrome', '/usr/share/icons/hicolor/16x16/apps/google-chrome-beta.png'},
+
+      }
+    }, {'办公',  {
+        {'WPS 文字', '/usr/bin/wps', '/usr/share/icons/hicolor/48x48/apps/wps-office-wpsmain.png' },
+      }
+    }, {'系统工具',  {
+        {'Roxterm', '/usr/bin/roxterm', '/usr/share/pixmaps/roxterm.xpm' },
+
+      }
+    },{'附件',  {
+        {'运行程序...', 'xfce4-appfinder --collapsed', '/usr/share/icons/gnome/16x16/actions/gtk-execute.png' },
+      }
+    }
+  }
+end
