a5c10d3ecc8e794611a4c5fdea7e62e2c0360108
etony C.F.AN <etony.an@gmail.com>
2015-11-19 10:52:52 +0800

    remove goagent

--- config/includes.chroot/TOOLS/proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo sh -c "echo nameserver 114.114.114.114  > /etc/resolv.conf "
-
-sudo sh -c "echo nameserver 202.98.192.67  >> /etc/resolv.conf "
-
-sleep 20s
-
-sudo /usr/bin/python /TOOLS/GoAgent/local/proxy.py >> /dev/null &
